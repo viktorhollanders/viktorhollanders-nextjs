01677f583deebcc5eac54237f5bb324df7a1435e
Viktor Hollanders <viktorhollanders@gmail.com>
2021-04-04 15:23:58 +0000

    update_app.js export default

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,27 @@
-export default ({ Component, pageProps }) => (
-  <div>
-    <Component {...pageProps} />
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        font-size: 18px;
-        line-height: 1.7;
-      }
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+          font-size: 18px;
+          line-height: 1.7;
+        }
 
-      * {
-        box-sizing: border-box;
-      }
+        * {
+          box-sizing: border-box;
+        }
 
-      .capped {
-        padding: 0 16px;
-      }
-    `}</style>
-  </div>
-)
+        .capped {
+          padding: 0 16px;
+        }
+      `}</style>
+    </div>
+  )
+}
