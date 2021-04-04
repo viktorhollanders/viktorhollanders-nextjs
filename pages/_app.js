544082c0fdfbab4d00667f48cb6856fbe015ec31
Viktor Hollanders <viktorhollanders@gmail.com>
2021-04-04 15:37:51 +0000

    moving global styles in to seperate style sheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,7 @@
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <Component {...pageProps} />
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-          font-size: 18px;
-          line-height: 1.7;
-        }
+import '../styles/global.css'
 
-        * {
-          box-sizing: border-box;
-        }
-
-        .capped {
-          padding: 0 16px;
-        }
-      `}</style>
-    </div>
-  )
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
